1b08ca679f16cddb83bd07bcbb32e1bce459d845
dtgithub <39837914+dangthieu123@users.noreply.github.com>
2022-07-01 01:38:48 +0700

    13-CRUD-DElETE/UPDATE complete

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -26,9 +26,27 @@ let deleteUser = async (req, res) => {
   return res.redirect("/");
 };
 
+let getEditPage = async (req, res) => {
+  let id = req.params.id;
+  let [user] = await pool.execute("SELECT * FROM users WHERE id =?", [id]);
+  return res.render("update.ejs", { dataUser: user[0] });
+};
+
+let updateUser = async (req, res) => {
+  let { firstName, lastName, email, address, id } = req.body;
+  await pool.execute(
+    "UPDATE users SET firstName =?, lastName=?, email= ?, address =? WHERE id= ?",
+    [firstName, lastName, email, address, id]
+  );
+  console.log("check request:", req.body);
+  return res.redirect("/");
+};
+
 module.exports = {
   getHomePage,
   getDetailPage,
   createNewUser,
   deleteUser,
+  getEditPage,
+  updateUser,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,8 @@ const initWebRoute = (app) => {
   router.get("/detail/user/:id", homeController.getDetailPage);
   router.post("/create-new-user", homeController.createNewUser);
   router.post("/delete-user", homeController.deleteUser);
+  router.get("/edit-user/:id", homeController.getEditPage);
+  router.post("/update-user", homeController.updateUser);
 
   router.get("/about", (req, res) => {
     res.send("Hi! About! I'm David");

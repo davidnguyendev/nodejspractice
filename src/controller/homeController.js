e4f319a029b0018e96e60a99c769fe7157721f54
dtgithub <39837914+dangthieu123@users.noreply.github.com>
2022-06-30 19:10:09 +0700

    10-createNAV-css-detail/edit/delete

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -3,7 +3,7 @@ import pool from "../configs/connectDB";
 let getHomePage = async (req, res) => {
   const [rows] = await pool.execute("SELECT * FROM users");
   console.log(rows);
-  return res.render("index.ejs", { userData: rows });
+  return res.render("index.ejs", { dataUser: rows });
 };
 
 module.exports = {
